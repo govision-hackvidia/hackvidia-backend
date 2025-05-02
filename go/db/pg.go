116b1be8861aef6f6760c099e50eec0c8117d5ab
richie_angjaya <maxnolimit@MaxNoLimit-Legion.>
2025-05-02 20:16:10 +0700

    added empty const to fill database url (line 14 - 20)

--- go/db/pg.go
+++ go/db/pg.go
@@ -2,6 +2,7 @@ package db
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/jackc/pgx/v5/pgxpool"
 )
@@ -10,15 +11,26 @@ type DB struct {
 	Pool *pgxpool.Pool
 }
 
-const postgresDBname = "govision_article"
+const (
+	username = ""
+	password = ""
+	hostname = ""
+	port     = ""
+	database = ""
+)
+
+// postgresql://<username>:<password>@<hostname>:<port>/<database>
 
 func NewDB() *DB {
 	// TODO: psql Connection
-	pool, err := pgxpool.New(context.Background(), postgresDBname)
+
+	DATABASE_URL := fmt.Sprintf("postgresql://%s:%s@%s:%s/%s", username, password, hostname, port, database)
+	pool, err := pgxpool.New(context.Background(), DATABASE_URL)
 	if err != nil {
 		return nil
 	}
 	return &DB{
 		Pool: pool,
 	}
+
 }
