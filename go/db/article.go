386c13b46e309ab66337d12ab9edc0e11c0f29d6
richie_angjaya <maxnolimit@MaxNoLimit-Legion.>
2025-05-02 17:31:14 +0700

    SelectArticleByID done but not optimized(returns many strings), SelectManyArticle halt

--- go/db/article.go
+++ go/db/article.go
@@ -3,6 +3,9 @@ package db
 import (
 	"context"
 	"fmt"
+	"math/rand"
+
+	"github.com/jackc/pgx/v5"
 )
 
 // table_name is article
@@ -31,10 +34,56 @@ func (d *DB) InsertArticle(id int, title string, date string, source string, rea
 
 func (d *DB) SelectManyArticle() {
 	// TODO: Select many Article
+
+	// select random from available article
+	// check the quantity of available articles
+	var nCount int64
+	err := d.Pool.QueryRow(context.Background(), "SELECT COUNT(*) FROM article").Scan(&nCount)
+	if err != nil {
+		fmt.Println("Query row failed!")
+	}
+
+	if nCount > 0 {
+		// there is article
+		// randomize value
+		randomSelectId := rand.Intn(int(nCount)-int(0)+1) + int(nCount)
+
+		// temporary variable for function return
+		var title, date, source, read_duration, photo, highlighted_text, description string
+
+		// select an article with randomSelectId result
+		err = d.Pool.QueryRow(context.Background(), "SELECT title, date, source, read_duration, photo, highlighted_text, description WHERE id = $1 LIMIT 1", randomSelectId).Scan(&title, &date, &source, &read_duration, &photo, &highlighted_text, &description)
+		if err != pgx.ErrNoRows {
+			fmt.Println("There is no row from select")
+		}
+	} else {
+		fmt.Println("There is no article available")
+	}
+
 }
 
-func (d *DB) SelectArticleByID() {
+func (d *DB) SelectArticleByID(article_id int) (string, string, string, string, string, string, string) {
 	// TODO: Select article by ID
+
+	// temporary variable for function return
+	var title, date, source, read_duration, photo, highlighted_text, description string
+
+	err := d.Pool.QueryRow(context.Background(), "SELECT title, date, source, read_duration, photo, highlighted_text, description WHERE id = $1 LIMIT 1", article_id).Scan(&title, &date, &source, &read_duration, &photo, &highlighted_text, &description)
+	if err != nil {
+		if err == pgx.ErrNoRows {
+			// There is not such article on that id
+			fmt.Printf("There is no article on id: %d", article_id)
+			return "", "", "", "", "", "", ""
+		} else {
+			// Failed to run query function
+			fmt.Println("Failed to execute :(")
+			return "", "", "", "", "", "", ""
+		}
+	} else {
+		fmt.Println("FOUND!! Will sent the data")
+		return title, date, source, read_duration, photo, highlighted_text, description
+	}
+
 }
 
 func (d *DB) DeleteArticle() {
